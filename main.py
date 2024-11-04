bda00a7f6a504226bccd0b791f54415881d258f3
Tushar Khandelwal <khandelwaltushar2002@gmail.com>
2024-11-05 00:45:30 +0530

    second commit

--- main.py
+++ main.py
@@ -1,57 +1,15 @@
-# import streamlit as st
-# from langchain_community.document_loaders import WebBaseLoader
-# from chains import Chain
-# from portfolio import Portfolio
-# from utils import clean_text
-
-# def create_streamlit_app(llm, portfolio, clean_text):
-#     st.title("🤝🏻 Referral Request Generator")
-#     url_input = st.text_input("Enter a URL:", value="https://www.amazon.jobs/en/jobs/2750545/sde-i-intern-6m-july-dec")
-#     submit_button = st.button("Submit")
-
-#     if submit_button:
-#         try:
-#             loader = WebBaseLoader([url_input])
-#             loaded_data = loader.load()
-            
-#             # Check if data is loaded
-#             if not loaded_data:
-#                 st.error("No content found at the provided URL.")
-#                 return
-            
-#             data = clean_text(loaded_data.pop().page_content)
-#             portfolio.load_portfolio()
-            
-#             # Extract jobs and check if any jobs are found
-#             jobs = llm.extract_jobs(data)
-#             if not jobs:
-#                 st.error("No job postings found in the extracted data.")
-#                 return
-
-#             for job in jobs:
-#                 skills = job.get('skills', [])
-#                 links = portfolio.query_links(skills)
-#                 email = llm.write_mail(job, links)
-#                 st.code(email, language='markdown')
-                
-#         except Exception as e:
-#             st.error(f"An Error Occurred: {e}")
-
-# if __name__ == "__main__":
-#     chain = Chain()
-#     portfolio = Portfolio()
-#     st.set_page_config(layout="wide", page_title="Cold Referral Generator", page_icon="🤝🏻")
-#     create_streamlit_app(chain, portfolio, clean_text)
 
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
+
+
 from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
 
 def create_streamlit_app(llm, portfolio, clean_text):
     st.title("🤝🏻 Referral Request Generator")
-    url_input = st.text_input("Enter a URL:", value="https://www.amazon.jobs/en/jobs/2750545/sde-i-intern-6m-july-dec")
+    url_input = st.text_input("Enter a URL:", value="https://jobs.apple.com/en-us/details/200554363/machine-learning-ai-internships")
     submit_button = st.button("Submit")
 
     if submit_button:
